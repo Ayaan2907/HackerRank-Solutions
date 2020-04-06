aa021d0fd70e64163a506ca902d2aad7a6a24ffc
Ayaan Kaifullah <60045274+Ayaan2907@users.noreply.github.com>
2020-04-06 22:50:04 +0530

    Update Compare the Triplets.cpp

--- Algorithms/Warmup/Compare the Triplets.cpp
+++ Algorithms/Warmup/Compare the Triplets.cpp
@@ -1,38 +1,15 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 
 int main(){
-    int a0;
-    int a1;
-    int a2;
+    int a0, a1, a2;
     cin >> a0 >> a1 >> a2;
-    int b0;
-    int b1;
-    int b2;
+    
+    int b0, b1, b2;
     cin >> b0 >> b1 >> b2;
+    
     int a = 0, b = 0;
     if(a0 > b0) {
     	a++;
